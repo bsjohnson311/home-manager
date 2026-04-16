10f3e02629ced80b1febe647ee1d75becb73a53a
bryce <bsjohnson311@gmail.com>
2026-04-15 21:17:26 -0700

    Added Nvidia driver

--- ubuntu-home/default.nix
+++ ubuntu-home/default.nix
@@ -6,7 +6,11 @@
 
   home.stateVersion = "25.11"; 
 
-  targets.genericLinux.gpu.enable = true;
+  targets.genericLinux.gpu.nvidia = {
+    enable = true;
+    version = "580.126.09";
+    sha256 = "sha256-TKxT5I+K3/Zh1HyHiO0kBZokjJ/YCYzq/QiKSYmG7CY=";
+  };
 
   news.display = "silent";
 
